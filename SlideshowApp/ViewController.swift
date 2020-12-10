8864c3e70fca918f293fade4cb6795fb154cb86f
yohei inokuchi <inoyoh@gmail.com>
2020-12-10 16:22:50 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by yohei on 2020/12/10.
+//  Copyright © 2020 yohei.inokuchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
